0ab43e717d7c20495ae0fa2b2a3ad1072933e8a7
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2026-04-25 16:52:08 +0530

    Add files via upload

---
+++ Functions/greet_function.c
@@ -0,0 +1,10 @@
+//1.Write a function void greet() that prints "Welcome to Embedded C".
+#include <stdio.h>
+
+void greet(){
+  printf("\"Welcome to Embedded C\"\n");
+}
+int main(){
+  greet();
+  return 0;
+}
