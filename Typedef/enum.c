1cd062d5a2bf8c89405f7dc98f444c39754e2cc5
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2026-05-18 21:10:05 +0530

    Create enum.c

---
+++ Typedef/enum.c
@@ -0,0 +1,9 @@
+//Create enum for days of week and print values.
+#include <stdio.h>
+enum week {SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY};
+int main(){
+  enum week days;
+  printf("SUNDAY = %d,MONDAY = %d,TUESDAY = %d,WEDNESDAY = %d,THURSDAY = %d,FRIDAY = %d,SATURDAY = %d\n",
+  SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY);
+  return 0;
+}
